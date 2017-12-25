928aaa21c6193b4440a2ee88e0d6952d9a21d428
erindy <erindmehmeti@gmail.com>
2017-12-25 18:16:08 +0100

    readded the missing files

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  @Input('is-favorite') isFavorite: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.isFavorite = !this.isFavorite
+  }
+
+}
