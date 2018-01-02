2f6c71cf0603667b8fc717a000acd6537b4b34d0
erindy <erindmehmeti@gmail.com>
2018-01-02 23:00:32 +0100

    added panel component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
-
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FavoriteComponent  {
   @Input('isFavorite') isSelected: boolean;
